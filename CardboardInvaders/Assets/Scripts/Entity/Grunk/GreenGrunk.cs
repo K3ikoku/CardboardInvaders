9a44ac4ce2a7187e73bdea656b7b98f0e5ffb0ad
Gustaf <wall.gustaf@gmail.com>
2016-11-22 17:11:56 +0100

    Started creating behaviortree for Grunk

--- CardboardInvaders/Assets/Scripts/Entity/Grunk/GreenGrunk.cs
+++ CardboardInvaders/Assets/Scripts/Entity/Grunk/GreenGrunk.cs
@@ -1,7 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Stats))]
 public class GreenGrunk : Entity
 {
+    private BehaviorTree m_bt;
+    private Blackboard m_bb;
+    private Composite m_root;
 
+    protected override void Start()
+    {
+        base.Start();
+
+        m_bb = new Blackboard();
+        m_bb.SetStats(m_stats);
+        m_bt = new BehaviorTree();
+        m_root = new Selector();
+
+        m_bt.SetRoot(m_root);
+        
+    }
 }
